0ef338b192bff1e53d2543219136fd83f0f55e28
Philippe Caron <philippe-caron@hotmail.com>
2019-12-13 23:11:56 -0500

    move and swap

--- inc/csv.h
+++ inc/csv.h
@@ -12,25 +12,25 @@ public:
     };
 
 public:
-    CSV(char *, bool = true);
+    CSV(char *filename, bool firstRowIsHeader= true);
     ~CSV();
 
-    void addColumn();
-    void addColumn(const char *columnName);
-    void moveColumn(int from, int to);
-    void moveColumn(const char *fromName, int to);
-    void swapColumn(int col1, int col2);
-    void swapColumn(const char *col1Name, const char *col2Name);
-    void insertColumn(int index);
-    void insertColumn(const char *columnName);
-    void deleteColumn(int index);
-    void deleteColumn(const char *columnName);
+    bool addColumn();
+    bool addColumn(const char *columnName);
+    bool moveColumn(int from, int to);
+    bool moveColumn(const char *fromName, int rel);
+    bool swapColumn(int col1, int col2);
+    bool swapColumn(const char *col1Name, const char *col2Name);
+    bool insertColumn(int index);
+    bool insertColumn(int index, const char *columnName);
+    bool deleteColumn(int index);
+    bool deleteColumn(const char *columnName);
 
-    void addRow();
-    void moveRow(int from, int to);
-    void swapRow(int row1, int row2);
-    void insertRow(int index);
-    void deleteRow(int index);
+    bool addRow();
+    bool moveRow(int from, int to);
+    bool swapRow(int row1, int row2);
+    bool insertRow(int index);
+    bool deleteRow(int index);
 
 
 private:
--- src/csv.cpp
+++ src/csv.cpp
@@ -14,13 +14,15 @@ CSV::CSV(char *filename, bool firstRowIsHeader)
 {
 }
 
-void CSV::addColumn()
+bool CSV::addColumn()
 {
-    addColumn(nullptr);
+    return addColumn(nullptr);
 }
 
-void CSV::addColumn(const char *columnName)
+bool CSV::addColumn(const char *columnName)
 {
+    bool success = true;
+    try {
     if (dim_h + 1 > cap_h) {
         int ncap;
         if (cap_h == 0) ncap = 1;
@@ -49,28 +51,134 @@ void CSV::addColumn(const char *columnName)
     }
     content[dim_h] = new char*[dim_v];
     dim_h++;
+    } catch (...) {
+        success = false;
+    }
+    return success;
 }
 
-void CSV::addRow()
+bool CSV::moveColumn(int from, int to)
 {
+    bool success = false;
+    if (from >= 0 && from < dim_h && to >= 0 && to < dim_h) {
+        if (from != to) {
+            char  *t_columnName = header[from];
+            char **t_column     = content[from];
+            if (from < to) {
+                for (int i = from; i < to; i++) {
+                    header[i]  = header[i + 1];
+                    content[i] = content[i + 1];
+                }
+            } else {
+                for (int i = from; i > to; i++) {
+                    header[i]  = header[i - 1];
+                    content[i] = content[i - 1];
+                }
+            }
+            header[to]  = t_columnName;
+            content[to] = t_column;
+        }
+        success = true;
+    }
+    return success;
+}
+
+bool CSV::moveColumn(const char *columnName, int rel)
+{
+    int from = column(columnName);
+    return moveColumn(from, from + rel);
+}
+
+bool CSV::swapColumn(int col1, int col2)
+{
+    bool success = false;
+    if (col1 >= 0 && col1 < dim_h && col2 >= 0 && col2 < dim_h) {
+        if (col1 != col2) {
+            char  *t_columnName = header[col1];
+            char **t_column     = content[col1];
+            header[col1]  = header[col2];
+            content[col1] = content[col2];
+            header[col2]  = t_columnName;
+            content[col2] = t_column;
+        }
+        success = true;
+    }
+    return success;
+}
+
+bool CSV::swapColumn(const char *col1Name, const char *col2Name)
+{
+    return swapColumn(column(col1Name), column(col2Name));
+}
+
+bool CSV::addRow()
+{
+    bool success = true;
+    try {
     if (dim_v + 1 > cap_v) {
         int ncap;
         if (cap_v == 0) ncap = 1;
         else            ncap = 2 * cap_v;
         for (int j = 0; j < dim_h; j++) {
-            char **column = new char*[ncap];
+            char **t_column = new char*[ncap];
             for (int i = 0;     i < cap_h; i++) {
-                column[i] = content[j][i];
+                t_column[i] = content[j][i];
             }
             for (int i = cap_h; i < ncap;  i++) {
-                column[i] = nullptr;
+                t_column[i] = nullptr;
             }
             delete[] content[j];
-            content[j] = column;
+            content[j] = t_column;
         }
         cap_v = ncap;
     }
     dim_v++;
+    } catch (...) {
+        success = false;
+    }
+    return success;
+}
+
+bool CSV::moveRow(int from, int to)
+{
+    bool success = false;
+    if (from >= 0 && from < dim_v && to >= 0 && to < dim_v) {
+        if (from != to) {
+            for (int j = 0; j < dim_h; j++)
+            {
+                char  *t_cell = content[j][from];
+                if (from < to) {
+                    for (int i = from; i < to; i++) {
+                        content[j][i] = content[j][i + 1];
+                    }
+                } else {
+                    for (int i = from; i > to; i++) {
+                        content[j][i] = content[j][i - 1];
+                    }
+                }
+                content[j][to] = t_cell;
+            }
+        }
+        success = true;
+    }
+    return success;
+}
+
+bool CSV::swapRow(int row1, int row2)
+{
+    bool success = false;
+    if (row1 >= 0 && row1 < dim_v && row2 >= 0 && row2 < dim_v) {
+        if (row1 != row2) {
+            for (int i = 0; i < dim_h; i++)
+            {
+                char  *t_cell = content[i][row1];
+                content[i][row1] = content[i][row2];
+                content[i][row2] = t_cell;
+            }
+        }
+        success = true;
+    }
+    return success;
 }
 
 int CSV::column(const char *columnName) {
